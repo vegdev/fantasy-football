8a46a9a70e10de2a4c934b553ef213f8fc68d5da
Kyle <kyle.gray@yoursole.com>
2023-02-05 12:48:58 -0800

    create new audio control components

---
+++ src/components/icons/Stop.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg
+    width="800px"
+    height="800px"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+  <path d="M6 6H18V18H6V6Z" fill="#000000"/>
+  </svg>
+</template>
