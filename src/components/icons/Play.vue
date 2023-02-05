cdd9c221496f21f9e75f378b7a1fb31af38be118
Kyle <kgrayallday@gmail.com>
2023-02-05 13:38:00 -0800

    update styling to icon components

--- src/components/icons/Play.vue
+++ src/components/icons/Play.vue
@@ -1,7 +1,7 @@
 <template>
   <svg
-    width="800px"
-    height="800px"
+    width="32px"
+    height="32px"
     viewBox="0 0 25 25"
     fill="#000000"
     xmlns="http://www.w3.org/2000/svg"
--- src/components/icons/Stop.vue
+++ src/components/icons/Stop.vue
@@ -1,7 +1,7 @@
 <template>
   <svg
-    width="800px"
-    height="800px"
+    width="32px"
+    height="32px"
     viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
