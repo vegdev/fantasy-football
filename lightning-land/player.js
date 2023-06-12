b94bd0d68f56fd575e9c2649851fef7846412d3c
vegdev <kgrayallday@gmail.com>
2023-06-12 00:31:41 -0700

    update functions

--- lightning-land/player.js
+++ lightning-land/player.js
@@ -1,13 +1,62 @@
-import tracksFile from './public/tracks.json' assert { type: 'json' };
+import tracksObject from './public/tracks.json' assert { type: 'json' };
+console.log('tracksObject:',tracksObject);
 
 // create the audio context
 const audioContext = new AudioContext();
+console.log('audio Context', audioContext);
 
 // get refrenece to the player element
-const llplayer = document.getElementById('lightning-land');
+const llplayer = document.getElementById('lightning-land-tracks');
+console.log('llplayer',llplayer);
 
-// get the track elements using the player element
-const htmlTracks = llplayer.querySelectorAll('.track');
+// get array of track elements
+const tracks = llplayer.children;
+console.log('tracks', tracks);
+
+// functon initAudio() {}
+
+
+function setPlayButton(idname, index) {
+  let play = document.querySelector(`div#${idname}.track .track-controls .track-play`)
+  play.addEventListener('click', () => {
+    // play the track
+    tracksObject[index].audio.play();
+    console.log('play clicked for', idname);
+  });
+}
+
+function setPauseButton(idname, index) {
+  let play = document.querySelector(`div#${idname}.track .track-controls .track-pause`)
+  play.addEventListener('click', () => {
+    // pause the track
+    tracksObject[index].audio.pause();
+    console.log('pause clicked for', idname);
+  });
+}
+
+function setStopButton(idname, index) {
+  let play = document.querySelector(`div#${idname}.track .track-controls .track-stop`)
+  play.addEventListener('click', () => {
+    // stop the track
+    tracksObject[index].audio.pause();
+    tracksObject[index].audio.currentTime = 0;
+    console.log('stop clicked for', idname);
+  });
+}
+
+// this function is complete just need to access the play function of the audio context?
+tracksObject.forEach((track, index) => {
+  let audio = new Audio(track.src[0]);
+  tracksObject[index].audio = audio;
+  setPlayButton(track.idname, index)
+  setPauseButton(track.idname, index)
+  setStopButton(track.idname, index)
+});
+
+console.log('new tracksObject', tracksObject);
+
+// use Element.closest to get the closest ancestor
+
 
 // how can we do this?
 // - we need to have event listeners on every play button at least
